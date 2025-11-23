931333cc8b6fd3c72e5f89abe6f6a5659dfa96d6
fanjatiana-dataS <fanjatianaanita@gmail.com>
2025-11-23 20:33:32 +0300

    sprint6_ter

--- src/main/java/utiles/ParamResolver.java
+++ src/main/java/utiles/ParamResolver.java
@@ -1,20 +1,35 @@
 package utiles;
 
 import jakarta.servlet.http.HttpServletRequest;
+import method_annotations.RequestParam;
+
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import org.apache.commons.beanutils.ConvertUtils;
 
 public class ParamResolver {
 
-    public static Object[] resolveArguments(Method method, HttpServletRequest request) throws IllegalArgumentException {
+    public static Object[] resolveArguments(Method method, HttpServletRequest request, RouteHandler handler) {
         Parameter[] parameters = method.getParameters();
         Object[] args = new Object[parameters.length];
 
         for (int i = 0; i < parameters.length; i++) {
             Parameter param = parameters[i];
             String paramName = param.getName();
-            String stringValue = request.getParameter(paramName);
+            RequestParam rp = param.getAnnotation(RequestParam.class);
+
+            if (rp != null && !rp.value().isEmpty()) {
+                paramName = rp.value(); 
+            }
+
+           String stringValue;
+
+           if (handler != null && handler.getPathVariable(paramName) != null) {
+                stringValue = handler.getPathVariable(paramName);
+           }
+            else {
+                stringValue = request.getParameter(paramName);
+            }
 
             if (stringValue == null || stringValue.isBlank()) {
                 throw new IllegalArgumentException(
--- src/main/java/utiles/RouteHandler.java
+++ src/main/java/utiles/RouteHandler.java
@@ -1,11 +1,14 @@
 package utiles;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RouteHandler {
 
     private final Class<?> clazz;
     private final Method method;
+    private final Map<String, String> pathVariables = new HashMap<>();
 
     public RouteHandler(Class<?> clazz, Method method) {
         this.clazz = clazz;
@@ -18,5 +21,13 @@ public class RouteHandler {
 
     public Method getMethod() {
         return this.method;
+    }
+
+    public void setPathVariable(String name, String value) {
+        pathVariables.put(name, value);
+    }
+
+    public String getPathVariable(String name) {
+        return pathVariables.get(name);
     }
 }
